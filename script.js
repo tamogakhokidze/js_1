a7013ad92093c9fd89cc98973d0cf2cf6cb45e8f
Tamar Gakhokidze <63312406+tamogakhokidze@users.noreply.github.com>
2023-02-22 19:21:18 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//1 task
+
+let a = 100;
+
+if (a < 50) {
+  console.log("ნაკლებია 50-ზე");
+} else if (a > 20) {
+  console.log("მეტია 20-ზე");
+} else {
+  console.log("შეცდომა");
+}
+//2 task
+
+let firstName = "მარიამ";
+
+if ((firstName = "მარიამ")) {
+  console.log(True);
+} else {
+  console.log(False);
+}
+
+//3 task
+
+let itemName = "მარიამ";
+
+switch (itemName) {
+  case "მარიამ":
+    console.log(True);
+    break;
+  default:
+    console.log(False);
+}
